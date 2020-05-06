f0bfcf22a7092ceeb8dce72cbca04286c51bc7ca
amanm4 <amanm4@localhost.localdomain>
2020-05-05 17:49:17 -0700

    first commit

---
+++ hw1/task4.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+####################################
+# Name: Aman Mohammed
+# CSE 391 - Spring 2020 
+# Homework 1 - Task 4
+####################################
+
+function problem1 {
+  # Type your answer to problem #1 below this line
+  cp MyProgram.java java/
+}
+
+function problem2 {
+  # Type your answer to problem #2 below this line
+  ls -l
+}
+
+function problem3 {
+  # Type your answer to problem #3 below this line
+  ls -lar /var
+}
+
+function problem4 {
+  # Type your answer to problem #4 below this line
+  mv Burrot.java Borat.java
+}
+
+function problem5 {
+  # Type your answer to problem #5 below this line
+  rm diff.html diff.css
+}
+
+function problem6 {
+  # Type your answer to problem #6 below this line
+  touch -m -t 202001010415 MyProgram.java
+}
+
+function problem7 {
+  # Type your answer to problem #7 below this line
+  ls *.css *.html
+}
+
+function problem8 {
+  # Type your answer to problem #8 below this line
+  cp *.txt java/
+}
+
+function problem9 {
+  # Type your answer to problem #9 below this line
+  diff lyrics.txt lyrics2.txt
+}
+
+function problem10 {
+  # Type your answer to problem #10 below this line
+  cat lyrics.txt 
+}
+
+function problem11 {
+  # Type your answer to problem #11 below this line
+  cat song*.txt
+}
+
+function problem12 {
+  # Type your answer to problem #12 below this line
+  head -n 7 animals.txt
+
+}
